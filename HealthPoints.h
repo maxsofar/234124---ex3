7f8e10f84d5b4099f0cc168842e67c4e25718bfe
Max <spiiked@gmail.com>
2022-05-21 17:33:11 +0300

    Completes tests now

--- HealthPoints.h
+++ HealthPoints.h
@@ -13,30 +13,29 @@ public:
 
     ~HealthPoints() = default;
 
-    HealthPoints operator+(int x);
-
     HealthPoints operator-(int x);
 
     HealthPoints& operator+=(int x);
 
     HealthPoints& operator-=(int x);
 
-    bool operator ==(const HealthPoints& rightSideHP) const;
+    bool operator ==(const HealthPoints& rightSide) const;
 
-    bool operator !=(const HealthPoints& rightSideHP) const;
+    bool operator !=(const HealthPoints& rightSide) const;
 
-    bool operator >(const HealthPoints& rightSideHP) const;
+    bool operator >(const HealthPoints& rightSide) const;
 
-    bool operator >=(const HealthPoints& rightSideHP) const;
+    bool operator >=(const HealthPoints& rightSide) const;
 
-    bool operator <(const HealthPoints& rightSideHP) const;
+    bool operator <(const HealthPoints& rightSide) const;
 
-    bool operator <=(const HealthPoints& rightSideHP) const;
+    bool operator <=(const HealthPoints& rightSide) const;
 
     class InvalidArgument {};
 
-    //to be deleted
-    int retHP();
+    //-----------//
+    friend HealthPoints operator+(int x, const HealthPoints& hp);
+
 private:
     int m_HP, m_maxHP;
 
@@ -45,29 +44,9 @@ private:
 
 HealthPoints::HealthPoints(int maxHP) {
     if (maxHP <= 0) {
-        throw InvalidArgument();
+        throw (InvalidArgument());
     }
     m_HP = m_maxHP = maxHP;
-}
-
-HealthPoints HealthPoints::operator+(int x) {
-    HealthPoints result(*this);
-    if (this->m_HP + x > this->m_maxHP) {
-        result.m_HP = this->m_maxHP;
-    } else {
-        result.m_HP += x;
-    }
-    return result;
-}
-
-HealthPoints HealthPoints::operator-(int x) {
-    HealthPoints result(*this);
-    if (this->m_HP - x  < 0) {
-        result.m_HP = 0;
-    } else {
-        result.m_HP -= x;
-    }
-    return result;
 }
 
 HealthPoints& HealthPoints::operator+=(int x) {
@@ -88,48 +67,68 @@ HealthPoints& HealthPoints::operator-=(int x) {
     return *this;
 }
 
-bool HealthPoints::operator==(const HealthPoints &rightSideHP) const {
-    if (this->m_HP == rightSideHP.m_HP) {
+HealthPoints operator+(int x, const HealthPoints& hp) {
+    HealthPoints result(hp);
+    if (hp.m_HP + x > hp.m_maxHP) {
+        result.m_HP = hp.m_maxHP;
+    } else {
+        result.m_HP += x;
+    }
+    return result;
+}
+
+HealthPoints HealthPoints::operator-(int x) {
+    HealthPoints result(*this);
+    if (this->m_HP - x  < 0) {
+        result.m_HP = 0;
+    } else {
+        result.m_HP -= x;
+    }
+    return result;
+}
+
+bool HealthPoints::operator==(const HealthPoints& rightSide) const {
+    if (this->m_HP == rightSide.m_HP) {
         return true;
     } else {
         return false;
     }
 }
 
-bool HealthPoints::operator!=(const HealthPoints &rightSideHP) const {
-    if (this->m_HP != rightSideHP.m_HP) {
+bool HealthPoints::operator!=(const HealthPoints& rightSide) const {
+    if (this->m_HP != rightSide.m_HP) {
         return true;
     } else {
         return false;
     }
 }
 
-bool HealthPoints::operator>(const HealthPoints &rightSideHP) const {
-    if (this->m_HP > rightSideHP.m_HP) {
+bool HealthPoints::operator>(const HealthPoints& rightSide) const {
+    if (this->m_HP > rightSide.m_HP) {
         return true;
     } else {
         return false;
     }
 }
 
-bool HealthPoints::operator>=(const HealthPoints &rightSideHP) const {
-    if (this->m_HP >= rightSideHP.m_HP) {
+bool HealthPoints::operator>=(const HealthPoints& rightSide) const {
+    if (this->m_HP >= rightSide.m_HP) {
         return true;
     } else {
         return false;
     }
 }
 
-bool HealthPoints::operator<(const HealthPoints &rightSideHP) const {
-    if (this->m_HP < rightSideHP.m_HP) {
+bool HealthPoints::operator<(const HealthPoints& rightSide) const {
+    if (this->m_HP < rightSide.m_HP) {
         return true;
     } else {
         return false;
     }
 }
 
-bool HealthPoints::operator<=(const HealthPoints &rightSideHP) const {
-    if (this->m_HP <= rightSideHP.m_HP) {
+bool HealthPoints::operator<=(const HealthPoints& rightSide) const {
+    if (this->m_HP <= rightSide.m_HP) {
         return true;
     } else {
         return false;
@@ -137,20 +136,8 @@ bool HealthPoints::operator<=(const HealthPoints &rightSideHP) const {
 }
 
 std::ostream& operator<<(std::ostream& os, const HealthPoints& healthPoints1) {
-    os << healthPoints1.m_HP << "(" << healthPoints1.m_maxHP << ")" << std::endl;
+    os << healthPoints1.m_HP << '(' << healthPoints1.m_maxHP << ')';
     return os;
 }
-
-
-int HealthPoints::retHP() {return m_HP;};
-
-int main() {
-    HealthPoints healthPoints2(120);
-    healthPoints2 -= 100;
-    std::cout << "You have: " << healthPoints2.retHP() << "HP" << std::endl;
-    healthPoints2 = healthPoints2 + 5;
-    std::cout << "You have: " << healthPoints2.retHP() << "HP" << std::endl;
-};
-
 
 #endif //HEALTHPOINTS_H
---
+++ Queue.h
@@ -0,0 +1,262 @@
+#ifndef EX3_QUEUE_H
+#define EX3_QUEUE_H
+
+#define CAPACITY 100
+
+template <class T>
+class Queue {
+public:
+    Queue();
+    ~Queue();
+    Queue(const Queue<T>& queue1);
+    Queue& operator=(const Queue<T>& queue1);
+
+    class Iterator;
+    Iterator begin();
+    Iterator end();
+
+    class ConstIterator;
+    ConstIterator begin() const;
+    ConstIterator end() const;
+
+    void pushBack(T x);
+    T& front(int x = 0);
+    void popFront();
+    int size();
+
+    class EmptyQueue{};
+
+private:
+    int m_capacity;
+    int m_size;
+    int m_front;
+    int m_back;
+    T* m_queue;
+};
+
+template <class T>
+Queue<T>::Queue() {
+    m_capacity = CAPACITY;
+    m_size = m_front = 0;
+    m_back = -1;
+    m_queue = new T[CAPACITY];
+}
+
+template <class T>
+Queue<T>::~Queue() {
+    delete[] m_queue;
+}
+
+template<class T>
+Queue<T>& Queue<T>::operator=(const Queue<T>& queue) {
+    if (this == &queue) {
+        return *this;
+    }
+    delete[] m_queue;
+    m_queue = new T[queue.m_sizesize];
+    m_size = queue.size;
+    m_capacity = queue.m_capacity;
+    m_front = queue.m_front;
+    m_back = queue.m_back;
+    for (int i = 0; i < m_size; ++i) {
+        m_queue[i] = queue.m_queue[i];
+    }
+    return *this;
+}
+
+template <class T>
+Queue<T>::Queue(const Queue<T>& queue1) {
+    m_capacity = queue1.m_capacity;
+    m_size = queue1.m_size;
+    m_front = queue1.m_front;
+    m_back = queue1.m_back;
+    m_queue = new T[queue1.m_capacity];
+    for (int i = 0; i < m_size; ++i) {
+        m_queue[i] = queue1.m_queue[i];
+    }
+}
+
+template <class T>
+void Queue<T>::pushBack(T x) {
+    m_back = (m_back + 1) % m_capacity;
+    m_queue[m_back] = x;
+    ++m_size;
+}
+
+template <class T>
+T& Queue<T>::front(int x) {
+    if (m_size == 0) {
+        throw EmptyQueue();
+    }
+    return m_queue[m_front];
+}
+
+template <class T>
+void Queue<T>::popFront() {
+    m_front = (m_front + 1) % m_capacity;
+    --m_size;
+}
+
+template <class T>
+int Queue<T>::size() {
+    return m_size;
+}
+
+template <class T, class Condition>
+Queue<T> filter(const Queue<T>& queue1, Condition c) {
+    Queue<T> result;
+
+    for (const T& element : queue1) {
+        if (c(element)) {
+            result.pushBack(element);
+        }
+    }
+    return result;
+}
+
+template <class T, class Function>
+void transform(Queue<T>& queue1, Function f) {
+    for (T& element : queue1) {
+        f(element);
+    }
+}
+
+template <class T>
+typename Queue<T>::Iterator Queue<T>::begin() {
+    return Iterator(this, m_front);
+}
+
+template <class T>
+typename Queue<T>::Iterator Queue<T>::end() {
+    return Iterator(this, m_back+1);
+}
+
+template <class T>
+typename Queue<T>::ConstIterator Queue<T>::begin() const{
+    return ConstIterator(this, m_front);
+}
+
+template <class T>
+typename Queue<T>::ConstIterator Queue<T>::end() const{
+    return ConstIterator(this, m_back+1);
+}
+
+//---------------------Iterator---------------------//
+template <class T>
+class Queue<T>::Iterator {
+public:
+    //const removed
+    T& operator*() const;
+    Iterator& operator++();
+    Iterator operator++(int);
+    bool operator==(const Iterator& iterator) const;
+    bool operator!=(const Iterator& iterator) const;
+    Iterator(const Iterator&) = default;
+    Iterator& operator=(const Iterator&) = default;
+
+    class InvalidOperation{};
+
+private:
+    const Queue<T>* queue;
+    int index;
+    Iterator(const Queue<T>* queue, int index);
+    friend class Queue<T>;
+    friend class Queue<T>::ConstIterator;
+};
+
+template <class T>
+Queue<T>::Iterator::Iterator(const Queue<T>* queue, int index) :
+    queue(queue), index(index) {
+}
+
+template <class T>
+//const removed
+T& Queue<T>::Iterator::operator*() const {
+    return queue->m_queue[index];
+}
+
+template <class T>
+typename Queue<T>::Iterator& Queue<T>::Iterator::operator++() {
+    if (this->index == queue->m_back+1) {
+        throw InvalidOperation();
+    }
+    ++index;
+    return *this;
+}
+
+template <class T>
+typename Queue<T>::Iterator Queue<T>::Iterator::operator++(int) {
+    Iterator result = *this;
+    ++*this;
+    return result;
+}
+
+template<class T>
+bool Queue<T>::Iterator::operator==(const Iterator& i) const {
+    return index == i.index;
+}
+
+template <class T>
+bool Queue<T>::Iterator::operator!=(const Iterator& i) const {
+    return !(*this == i);
+}
+
+//--------------------ConstIterator-------------------//
+template <class T>
+class Queue<T>::ConstIterator {
+public:
+    //const removed
+    T& operator*() const;
+    ConstIterator& operator++();
+    ConstIterator operator++(int);
+    bool operator==(const ConstIterator& iterator) const;
+    bool operator!=(const ConstIterator& iterator) const;
+    ConstIterator(const ConstIterator&) = default;
+    ConstIterator& operator=(const ConstIterator&) = default;
+
+    class InvalidOperation{};
+
+private:
+    const Queue<T>* queue;
+    int index;
+    ConstIterator(const Queue<T>* queue, int index);
+    friend class Queue<T>;
+};
+
+template <class T>
+Queue<T>::ConstIterator::ConstIterator(const Queue<T>* queue, int index) :
+        queue(queue), index(index) {
+}
+
+template <class T>
+T& Queue<T>::ConstIterator::operator*() const {
+    return queue->m_queue[index];
+}
+
+template <class T>
+typename Queue<T>::ConstIterator& Queue<T>::ConstIterator::operator++() {
+    if (this->index == queue->m_back+1) {
+        throw InvalidOperation();
+    }
+    ++index;
+    return *this;
+}
+
+template <class T>
+typename Queue<T>::ConstIterator Queue<T>::ConstIterator::operator++(int) {
+    ConstIterator result = *this;
+    ++*this;
+    return result;
+}
+
+template<class T>
+bool Queue<T>::ConstIterator::operator==(const ConstIterator& i) const {
+    return index == i.index;
+}
+
+template <class T>
+bool Queue<T>::ConstIterator::operator!=(const ConstIterator& i) const {
+    return !(*this == i);
+}
+
+#endif //EX3_QUEUE_H
